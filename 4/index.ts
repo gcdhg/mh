3a112c37a28ad17b40deeb181096354edcdb9479
gcdhg <gcdhg@outlook.com>
2023-03-03 01:57:49 +0300

    fixed args

--- 4/index.ts
+++ 4/index.ts
@@ -5,10 +5,10 @@ import { retry } from "./util/retry.util";
 const retryLimit: number = 5;
 
 const fetchData = (url: string, timeoutMs = 5000): Promise<any> => {
-    return Promise.race([retry(fetch, url, retryLimit), delay(timeoutMs)]).then((res) => {
+    return Promise.race([retry(fetch, this, [url], retryLimit), delay(timeoutMs)]).then((res) => {
         if (res instanceof Error) throw res;
         return res;
     });
 };
 
-fetchData("https://github.com/", 0);
+fetchData("https://github.com/");
