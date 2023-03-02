5c6ede32a614edbc35d5ed6004306b3f2a31949e
gcdhg <gcdhg@outlook.com>
2023-03-03 00:52:55 +0300

    added check valid

--- 2/index.ts
+++ 2/index.ts
@@ -1,5 +1,11 @@
 import assert from "assert";
 
+/**
+ * найти не закрытый тэг
+ *
+ * @param str
+ * @returns
+ */
 function findBrokenTag(str: string): boolean | string {
     const tags: RegExpMatchArray | null = str.match(/(?<=<)(.+?)(?=>)/gm);
 
@@ -19,6 +25,50 @@ function findBrokenTag(str: string): boolean | string {
     return tags[res] || true;
 }
 
+/**
+ * с проверкой валидности верстки
+ *
+ * @param str
+ * @returns
+ */
+function findBrokenTagV2(str: string): boolean | string {
+    const tags: RegExpMatchArray | null = str.match(/(?<=<)(.+?)(?=>)/gm);
+
+    if (!tags) return true;
+
+    const mapping: Record<string, number[]> = {};
+
+    let isValid: boolean = true;
+    const check: string[] = [];
+
+    tags.forEach((tag, index) => {
+        const isClose = tag[0] == "/";
+        const mainTag = isClose ? tag.slice(1) : tag;
+
+        let lastOpen = check[check.length - 1];
+
+        if (isValid) {
+            if (isClose) {
+                if (lastOpen !== mainTag) isValid = false;
+                else check.pop();
+            } else check.push(tag);
+        }
+
+        if (!isClose) {
+            mapping[tag] ||= [];
+            mapping[tag].push(index);
+        } else mapping[mainTag]?.pop();
+    });
+
+    const res = Math.min(...Object.values(mapping).map((el) => Math.min(...el)));
+
+    return tags[res] || isValid;
+}
+
 assert.equal(findBrokenTag("<div><span></span></div>"), true);
 assert.equal(findBrokenTag("<div><span></Span></div>"), "span");
-assert.equal(findBrokenTag("<div><span><span><span><div></span></span></div>"), "span");
+assert.equal(findBrokenTag("<div><span><span><span></span></span></div>"), "span");
+
+assert.equal(findBrokenTagV2("<div><span></div></span>"), false);
+assert.equal(findBrokenTagV2("<div><span></div>"), "span");
+assert.equal(findBrokenTagV2("<div><span></span></div>"), true);
